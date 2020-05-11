544e7b25155dd934757c1bae9d22e6dc6007795b
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-11 12:47:12 +0530

    Functionality added

--- landling-page/js/app.js
+++ landling-page/js/app.js
@@ -17,12 +17,15 @@
  * Define Global Variables
  *
  */
+const sections = document.querySelectorAll("section");
+const navbar = document.querySelector("#navbar__list");
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
+function activeElement() {}
 
 /**
  * End Helper Functions
@@ -31,10 +34,64 @@
  */
 
 // build the nav
+function addSections() {
+  for (let sect of sections) {
+    let section = document.createElement("li");
+    section.className = "menu__link";
+    section.dataset.nav = sect.id;
+    section.innerText = sect.dataset.nav;
+    navbar.appendChild(section);
+  }
+}
 
 // Add class 'active' to section when near top of viewport
+function setActive() {
+  window.addEventListener("scroll", function (event) {
+    maxSection = sections[0];
+    minVal = 100000;
+    for (const all__items of sections) {
+      let bounding = all__items.getBoundingClientRect();
+      if ((bounding.top > -400) & (bounding.top < minVal)) {
+        minVal = bounding.top;
+        maxSection = all__items;
+      }
+    }
+    maxSection.classList.add("your-active-class");
+    // set other sections as inactive
+    for (let all__item of sections) {
+      if (
+        (all__item.id != maxSection.id) &
+        all__item.classList.contains("your-active-class")
+      ) {
+        all__item.classList.remove("your-active-class");
+      }
+    }
+    // set corresponding header style
+    const active = document.querySelector(
+      'li[data-nav="' + maxSection.id + '"]'
+    );
+    active.classList.add("active__link");
+    // remove from other headers
+    const headers = document.querySelectorAll(".menu__link");
+    for (let all__item of headers) {
+      console.log(all__item);
+      if (
+        (all__item.dataset.nav != active.dataset.nav) &
+        all__item.classList.contains("active__link")
+      ) {
+        all__item.classList.remove("active__link");
+      }
+    }
+  });
+}
 
 // Scroll to anchor ID using scrollTO event
+function scrollOnClick() {
+  navbar.addEventListener("click", function (event) {
+    const clicked = document.querySelector("#" + event.target.dataset.nav);
+    clicked.scrollIntoView();
+  });
+}
 
 /**
  * End Main Functions
@@ -43,7 +100,8 @@
  */
 
 // Build menu
-
+addSections();
 // Scroll to section on link click
-
+scrollOnClick();
 // Set sections as active
+setActive();
