e2adab03983f021a63b34420c05b11358ed833e5
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-01 12:12:00 +0530

    Styles Added

--- projects/evaluate-news-nlp/src/client/index.js
+++ projects/evaluate-news-nlp/src/client/index.js
@@ -1,7 +1,8 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
+import "./styles/resets.scss";
 
 console.log(checkForName);
 
-alert("I EXIST")
+alert("I EXIST");
 console.log("CHANGE!!");
